614dd0cbc0676441611e3de1bae71fa9e0c7bd90
Peomarder <36908523+Peomarder@users.noreply.github.com>
2025-06-11 02:30:02 -0400

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,10 @@ const Navbar = ({ currentUser }) => {
 return (
 <nav>
 <ul>
-<li><Link to="/">Home</Link></li>
-<li><Link to="/detail">Detail</Link></li>
-<li><Link to="/form">Form</Link></li>
+<li><Link to="/landing">Home</Link></li>
+<li><Link to="/">Detail</Link></li>
+<li><Link to="/add">Form</Link></li>
+<li><Link to="/detail">LockDetail</Link></li>
 <li>{currentUser ? `Logged in as: ${currentUser}` : 'Not logged in'}</li>
 </ul>
 </nav>
