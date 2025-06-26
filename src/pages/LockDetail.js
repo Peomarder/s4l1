735d17d6fe135ef1e08ae56fdb44d373ca57183b
Peomarder <36908523+Peomarder@users.noreply.github.com>
2025-06-26 17:34:48 +0300

    Add files via upload

--- src/pages/LockDetail.js
+++ src/pages/LockDetail.js
@@ -14,7 +14,7 @@ const LockDetail = () => {
     const fetchLockDetails = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`http://localhost:5000/api/locks/${lockId}`);
+        const response = await fetch(`http://217.71.129.139:4821/api/locks/${lockId}`);
         
         if (!response.ok) {
           throw new Error(`Lock not found (ID: ${lockId})`);
